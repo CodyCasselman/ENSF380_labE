080039df056c171fdeb16c09fa9b001d24e85984
Martin Liu <martin.liu@ucalgary.ca>
2024-02-29 15:27:37 -0700

    updated args for interface

--- edu/ucalgary/oop/IToDoList.java
+++ edu/ucalgary/oop/IToDoList.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 public interface IToDoList {
     public void addTask(Task task);
-    public void deleteTask(String taskId);
-    public void completeTask(String taskId);
+    public void deleteTask(String id);
+    public void completeTask(String id);
     public void editTask(String taskId, String title, boolean isCompleted);
     public void undo();
     public List<Task> listTasks();
