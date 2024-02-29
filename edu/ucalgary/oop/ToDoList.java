993ecc8aff5be631a1a27e41706adec4268962fe
Martin Liu <martin.liu@ucalgary.ca>
2024-02-29 14:48:34 -0700

    Update ToDoList.java

--- edu/ucalgary/oop/ToDoList.java
+++ edu/ucalgary/oop/ToDoList.java
@@ -1,5 +1,15 @@
 package edu.ucalgary.oop;
 
-public class ToDoList {
-    
+import java.util.List;
+import java.util.Stack;
+import java.util.*;
+
+public class ToDoList implements IToDoList{
+    private List<Task> tasks = new ArrayList<Task>();
+    private Stack<List<Task>> history;
+
+    @Override
+    public void editTask(Task task) {
+        
+    }
 }
